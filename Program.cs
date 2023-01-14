75bfb7f706e9061293a04f758b1ed08e9a7fcf22
aldairandre <aldairandre99@gmail.com>
2023-01-14 18:36:15 +0000

    ➕ Manipulating strings
    Using native method such as Replace ToUpper ToLower

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
-﻿string greeting = "   Hello World Aldair   ";
-Console.WriteLine($"[{greeting}]");
+﻿string sayHello = "Hello World";
+Console.WriteLine($"{sayHello}");
 
-string trimmedGreeting = greeting.TrimStart();
-Console.WriteLine($"[{trimmedGreeting}]");
-
-trimmedGreeting = greeting.TrimEnd();
-Console.WriteLine($"[{trimmedGreeting}]");
-
-trimmedGreeting = greeting.Trim();
-Console.WriteLine($"[{trimmedGreeting}]");
+sayHello = sayHello.Replace("Hello","Greeting");
+Console.WriteLine(sayHello);
+Console.WriteLine("");
+Console.WriteLine("=== Upper and Lower ===");
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
