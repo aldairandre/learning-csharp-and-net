061b6768daccd85df6ba136bf3a8248abb907a28
aldairandre <aldairandre99@gmail.com>
2023-01-14 11:55:01 +0000

    ➕ Name and Height
    little program using some basic types

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿double height;
+string name;
+
+Console.WriteLine("");
+Console.WriteLine("======== CADAST FORM ========");
+Console.WriteLine("");
+Console.Write("What is your name: ");
+name = Console.ReadLine();
+Console.WriteLine("");
+Console.Write("What is your Height: ");
+height = double.Parse(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("======== DATAS ========");
+Console.WriteLine("");
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Height: {height}");
