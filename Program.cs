f436b25e210c24a564a40fb1ce24321c642f5f0b
aldairandre <aldairandre99@gmail.com>
2023-01-15 04:37:23 +0000

    ➕ Tabuada

--- Program.cs
+++ Program.cs
@@ -1,13 +1,8 @@
 ﻿Console.WriteLine("");
-Console.WriteLine("======== What is your age? ========");
+Console.Write("Digite um numero: ");
+int numberT =  int.Parse(Console.ReadLine());
 Console.WriteLine("");
-Console.Write("What is your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("");
-Console.Write("What's year were you born: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2023 - year;
-Console.Clear();
-Console.WriteLine("");
-Console.WriteLine($"Hellow {name}, your are {age}");
-Console.WriteLine("");
+Console.WriteLine("=== TABUADA ===");
+for(var n = 1; n <= 12 ; n++) {
+  Console.WriteLine($"{numberT} X {n} =  {numberT * n}");
+}
