fd47b39d53683978a05c6c17137e36cf6b0a08d5
aldairandre <aldairandre99@gmail.com>
2023-01-17 22:55:16 +0000

    ➕ Type Casting
    Converting types

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
-﻿// C# Data Types
+﻿// C# Type Casting
+/*
+  Type casting is when you assign a value of one data type to another type.
 
-int myNum = 5;               // Integer (whole number)
-double myDoubleNum = 5.99D;  // Floating point number
-float myFloatNum = 4.00F;    // Floating point number 
-char myLetter = 'D';         // Character
-bool myBool = true;          // Boolean
-string myText = "Hello";     // String
+    In C#, there are two types of casting:
 
+    Implicit Casting (automatically) - converting a smaller type to a larger type size
+    char -> int -> long -> float -> double
 
-Console.WriteLine($"{myDoubleNum}");
+    Explicit Casting (manually) - converting a larger type to a smaller size type
+    double -> float -> long -> int -> char
+
+*/
+
+// Implicit casting is done automatically when passing a smaller size type to a larger size type:
+
+int myInt = 9;
+double myDouble = (double) myInt;
+string myString =  Convert.ToString(myInt);
+
+char firstLettarName = 'A';
+int firstLettarNameC = firstLettarName; 
+firstLettarName = (char) firstLettarNameC;
+
+Console.WriteLine();
+Console.WriteLine("========= CHAR TO INT ===========");
+Console.WriteLine($"{firstLettarName}\n{firstLettarNameC}");
+Console.WriteLine("========= INT TO DOUBLE ===========");
+Console.Write($"{myInt}\n{myDouble}");
+Console.WriteLine();
+Console.WriteLine("========= INT TO STRING ===========");
+Console.Write($"{myInt+2}\n{myString+2}");
+Console.WriteLine();
