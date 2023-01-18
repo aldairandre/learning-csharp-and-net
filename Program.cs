0db9bcd4a58d206c22193b4a58ce0bfd82de391e
aldairandre <aldairandre99@gmail.com>
2023-01-18 14:57:41 +0000

    ➕ For loop

--- Program.cs
+++ Program.cs
@@ -1,13 +1,9 @@
-﻿Console.WriteLine("");
-Console.WriteLine("======== What is your age? ========");
-Console.WriteLine("");
-Console.Write("What is your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("");
-Console.Write("What's year were you born: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2023 - year;
-Console.Clear();
-Console.WriteLine("");
-Console.WriteLine($"Hellow {name}, your are {age}");
-Console.WriteLine("");
+﻿/*
+  C# For Loop
+    When you know exactly how many times you want to loop through a block of code, use the for loop instead of a while loop:
+*/
+
+for (int i = 0; i < 5; i++) 
+{
+  Console.WriteLine(i);
+}
