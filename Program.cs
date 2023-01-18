1aa3718816953ae5d81d14061ab67ab35b36c056
aldairandre <aldairandre99@gmail.com>
2023-01-18 14:52:41 +0000

    ➕ While loop

--- Program.cs
+++ Program.cs
@@ -1,13 +1,8 @@
-﻿Console.WriteLine("");
-Console.WriteLine("======== What is your age? ========");
-Console.WriteLine("");
-Console.Write("What is your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("");
-Console.Write("What's year were you born: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2023 - year;
-Console.Clear();
-Console.WriteLine("");
-Console.WriteLine($"Hellow {name}, your are {age}");
-Console.WriteLine("");
+﻿// The while loop loops through a block of code as long as a specified condition is True:
+
+int i = 0;
+while (i < 5) 
+{
+  Console.WriteLine(i);
+  i++;
+}
