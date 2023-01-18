a5121c781d24c3bd0e0037b44c0cd5556a87bac9
aldairandre <aldairandre99@gmail.com>
2023-01-18 15:07:15 +0000

    ➕ Break statement

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿Console.WriteLine("");
-Console.WriteLine("======== What is your age? ========");
-Console.WriteLine("");
-Console.Write("What is your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("");
-Console.Write("What's year were you born: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2023 - year;
-Console.Clear();
-Console.WriteLine("");
-Console.WriteLine($"Hellow {name}, your are {age}");
-Console.WriteLine("");
+﻿/*
+  C# Break
+    You have already seen the break statement used in an earlier chapter of this tutorial. It was used to "jump out" of a switch statement.
+
+    The break statement can also be used to jump out of a loop.
+
+    This example jumps out of the loop when i is equal to 4:
+
+*/
+
+for (int i = 0; i < 10; i++) 
+{
+  if (i == 4) 
+  {
+    break;
+  }
+  Console.WriteLine(i);
+}
