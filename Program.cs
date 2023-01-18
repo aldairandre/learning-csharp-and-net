cece41599b5da8fddbda09200613174f6e69c05b
aldairandre <aldairandre99@gmail.com>
2023-01-18 14:54:04 +0000

    ➕ Do While loop

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿// The while loop loops through a block of code as long as a specified condition is True:
+﻿/*
+  The Do/While Loop
+    The do/while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+*/
 
 int i = 0;
-while (i < 5) 
+do 
 {
   Console.WriteLine(i);
   i++;
 }
+while (i < 5);
