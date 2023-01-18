5ced14a37605c6835d3c6e6298e68ec8ddce6d83
aldairandre <aldairandre99@gmail.com>
2023-01-18 12:54:49 +0000

    ➕ If Statement
    simple if statement

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
-﻿Console.WriteLine("");
-Console.WriteLine("======== What is your age? ========");
-Console.WriteLine("");
-Console.Write("What is your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("");
-Console.Write("What's year were you born: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2023 - year;
-Console.Clear();
-Console.WriteLine("");
-Console.WriteLine($"Hellow {name}, your are {age}");
-Console.WriteLine("");
+﻿if (20 > 18) {
+  Console.WriteLine("20 is greater than 18");
+}
