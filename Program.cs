29c7f9d91736d81063b94afe6b9a53af7a15a5e2
aldairandre <aldairandre99@gmail.com>
2023-01-18 19:44:24 +0000

    ➕ Basic of array

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
-﻿Console.WriteLine("");
-Console.WriteLine("======== What is your age? ========");
-Console.WriteLine("");
-Console.Write("What is your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("");
-Console.Write("What's year were you born: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2023 - year;
-Console.Clear();
-Console.WriteLine("");
-Console.WriteLine($"Hellow {name}, your are {age}");
-Console.WriteLine("");
+﻿/*
+  Create an Array
+    Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+    To declare an array, define the variable type with square brackets:
+*/
+
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+int[] myNum = {10,20,30,40}; 
+Console.WriteLine();
+Console.WriteLine("Length of cars array: "+cars.Length);
+Console.WriteLine("First element: "+cars[0]);
+Console.WriteLine();
+Console.WriteLine("Length of myNum array: "+cars.Length);
+Console.WriteLine("First element: "+myNum[0]);
+Console.WriteLine();
