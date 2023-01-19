087cbb5c4f20ca85b913918fd17acc1a0226e577
aldairandre <aldairandre99@gmail.com>
2023-01-19 12:40:06 +0000

    ➕ Parameters and Arguments

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
-﻿static void MyMethod(){
-  Console.WriteLine("I just got executed");
+﻿string name;
+int yearBirth;
+static void Greeting(string name,int yearBirth){
+  Console.Clear();
+  Console.WriteLine();
+  Console.WriteLine($"Hello, {name} and you're {2023 - yearBirth} years old");
+  Console.WriteLine();
 }
-
-MyMethod();
+Console.WriteLine();
+Console.Write("What is your name: ");
+name = Console.ReadLine();
+Console.WriteLine();
+Console.Write("What year were you born: ");
+yearBirth = int.Parse(Console.ReadLine());
+Greeting(name,yearBirth);
