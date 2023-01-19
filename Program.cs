5b410e47d6b430f1e4319f9727d2e3126ccb2f04
aldairandre <aldairandre99@gmail.com>
2023-01-19 06:39:36 +0000

    ➕ Loop through an Array
    For loop

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
-﻿/*
-  Create an Array
-    Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+﻿// Loop Through an Array
 
-    To declare an array, define the variable type with square brackets:
-*/
+string [] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 
-string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-int[] myNum = {10,20,30,40}; 
-Console.WriteLine();
-Console.WriteLine("Length of cars array: "+cars.Length);
-Console.WriteLine("First element: "+cars[0]);
-Console.WriteLine();
-Console.WriteLine("Length of myNum array: "+cars.Length);
-Console.WriteLine("First element: "+myNum[0]);
-Console.WriteLine();
+for(int i = 0; i < cars.Length;i++){
+  Console.WriteLine(cars[i]);
+}
