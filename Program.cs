8ee1268dd08b06e4a9dddd70d45e5ab682763ff9
aldairandre <aldairandre99@gmail.com>
2023-01-19 10:12:38 +0000

    ➕ Extending with labrary linq

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿// Sort an Array
-// There are many array methods available, for example Sort(), which sorts an array alphabetically or in an ascending order:
+﻿// System.Linq
+using System.Linq;
+
 
 int [] myNum = {20,10,30 ,60, 40};
-
+Console.WriteLine();
+Console.Write("Array -> [ ");
 Array.Sort(myNum);
-foreach(int car in myNum){
-  Console.WriteLine(car);
+foreach(int num in myNum){
+  Console.Write($"{num} ");
 }
+Console.Write("]");
+Console.WriteLine();
+Console.WriteLine($"Max value in array : {myNum.Max()}"); // returns the largest value
+Console.WriteLine($"Min value in array : {myNum.Min()}"); // returns the smallest value
+Console.WriteLine($"Sum : {myNum.Sum()}");               // returns the sum of elements
+Console.WriteLine();
