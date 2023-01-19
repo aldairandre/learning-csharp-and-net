c7be02402a2f422e6244f30909b83adc51a2f8c5
aldairandre <aldairandre99@gmail.com>
2023-01-19 06:41:17 +0000

    ➕ Loop through an Array
    Foreach loop

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 
 string [] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 
-for(int i = 0; i < cars.Length;i++){
-  Console.WriteLine(cars[i]);
+foreach(string car in cars){
+  Console.WriteLine(car);
 }
