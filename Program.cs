d49c3857eea3846875912caebb908a2d982d0cc7
aldairandre <aldairandre99@gmail.com>
2023-01-19 12:06:13 +0000

    ➕ Creating and Calling a Method

--- Program.cs
+++ Program.cs
@@ -1,13 +1,5 @@
-﻿Console.WriteLine("");
-Console.WriteLine("======== What is your age? ========");
-Console.WriteLine("");
-Console.Write("What is your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("");
-Console.Write("What's year were you born: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2023 - year;
-Console.Clear();
-Console.WriteLine("");
-Console.WriteLine($"Hellow {name}, your are {age}");
-Console.WriteLine("");
+﻿static void MyMethod(){
+  Console.WriteLine("I just got executed");
+}
+
+MyMethod();
