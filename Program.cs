a308db5c93469bc421a72411ccdad6731428b5f5
aldairandre <aldairandre99@gmail.com>
2023-01-19 16:09:02 +0000

    ➕ Method Parameters

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿string name;
 int yearBirth;
-static void Greeting(string name,int yearBirth){
+
+static string Greeting(string name,int yearBirth){
   Console.Clear();
   Console.WriteLine();
-  Console.WriteLine($"Hello, {name} and you're {2023 - yearBirth} years old");
-  Console.WriteLine();
+  return $"Hello, {name} and you're {2023 - yearBirth} years old";
 }
+
 Console.WriteLine();
 Console.Write("What is your name: ");
 name = Console.ReadLine();
 Console.WriteLine();
 Console.Write("What year were you born: ");
 yearBirth = int.Parse(Console.ReadLine());
-Greeting(name,yearBirth);
+Console.WriteLine(Greeting(name,yearBirth));
+Console.WriteLine();
