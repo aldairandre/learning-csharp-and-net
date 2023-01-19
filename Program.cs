232d6f5d1c34c47d01d98b91a8f72a283fb31ccc
aldairandre <aldairandre99@gmail.com>
2023-01-19 09:54:35 +0000

    ➕ Sort method to arrays
    array of strings

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿// Loop Through an Array
+﻿// Sort an Array
+// There are many array methods available, for example Sort(), which sorts an array alphabetically or in an ascending order:
 
 string [] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 
+Array.Sort(cars);
 foreach(string car in cars){
   Console.WriteLine(car);
 }
