13f0d4570eb81ca28b57942bfa60e427b7824077
aldairandre <aldairandre99@gmail.com>
2023-01-22 16:19:00 +0000

    🎨 Constructors

--- Car.cs
+++ Car.cs
@@ -2,5 +2,9 @@ class Car {
   public string model;
   public string color;
   public int year;
-  
+  public Car(string nameModal,string nameColor,int releaseYear) {
+    model = nameModal;
+    color = nameColor;
+    year = releaseYear;
+  }
 }
--- Program.cs
+++ Program.cs
@@ -5,20 +5,14 @@
     static void Main(string[] args)
     {
       
-      Car ford = new Car();
-      ford.model = "Mustabg";
-      ford.color = "Yellow";
-      ford.year = 1969;
+      Car ford = new Car("Mustang","Yellow",1969);
 
-      Car opel = new Car();
-      opel.model = "Astra";
-      opel.color = "Black";
-      opel.year = 2005;
+      Car opel = new Car("Astra","Black",2005);
 
       Console.WriteLine();
       Console.WriteLine("=== CARS IN DB ===");
-      Console.WriteLine(ford.color);
-      Console.WriteLine(opel.color);
+      Console.WriteLine(ford.model);
+      Console.WriteLine(opel.model);
       Console.WriteLine();
     }
   }
